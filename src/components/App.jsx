21e2955e692ee7be9902aee69bb9c01217e3d205
jadedInsomnia89 <jbcrumley1@gmail.com>
2021-08-12 10:32:13 -0500

    initial commit with components, gitignore, eslint

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Footer from './Footer';
+import Note from './Note';
+import Header from './Header';
+import notes from '../notes';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        ©
+        {' '}
+        {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
